ca1e54e44e76824a135837e7bfe8df5f1d12e615
netzon-eladio <eladio.sareno@netzontech.com>
2020-11-14 02:44:36 +0800

    chore(eol): End of lecture

--- src/com/luv2code/springdemo/config/ConfigFortuneService.java
+++ src/com/luv2code/springdemo/config/ConfigFortuneService.java
@@ -1,12 +1,17 @@
 package com.luv2code.springdemo.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.luv2code.springdemo.FortuneService;
 
 public class ConfigFortuneService implements FortuneService {
+	
+	@Value("${config.fortune}")
+	String fortune;
 
 	@Override
 	public String getFortune() {
-		return "ConfigFortuneService#getFortune()";
+		return this.fortune;
 	}
 
 }
--- src/com/luv2code/springdemo/config/MyConfig.java
+++ src/com/luv2code/springdemo/config/MyConfig.java
@@ -2,11 +2,13 @@ package com.luv2code.springdemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import com.luv2code.springdemo.Coach;
 import com.luv2code.springdemo.FortuneService;
 
 @Configuration
+@PropertySource("classpath:sport.properties")
 public class MyConfig {
 	
 	@Bean
