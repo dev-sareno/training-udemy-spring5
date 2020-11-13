9fdec59b6322c8931397b78d8cc91a85f631f125
netzon-eladio <eladio.sareno@netzontech.com>
2020-11-13 22:08:41 +0800

    chore(eol): End of lecture

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -3,6 +3,8 @@ package com.luv2code.springdemo;
 public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	private String emailAddress;
+	private String team;
 
 	@Override
 	public String getDailyWorkout() {
@@ -21,6 +23,22 @@ public class CricketCoach implements Coach {
 
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
 	}
 
 }
--- src/com/luv2code/springdemo/HelloSpringApp.java
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -7,9 +7,9 @@ public class HelloSpringApp {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-		Coach cricketCoach = context.getBean("cricketCoach", Coach.class);
-
-		System.out.println("cricketCoach=" + cricketCoach.getDailyFortune());
+		CricketCoach cricketCoach = context.getBean("cricketCoach", CricketCoach.class);
+		System.out.println("emailAddress=" + cricketCoach.getEmailAddress());
+		System.out.println("team=" + cricketCoach.getTeam());
 		
 		context.close();
 	}
