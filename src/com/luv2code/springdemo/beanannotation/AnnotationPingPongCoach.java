3eb56864cdb0f172e917e3f80ca4b8dd1b1497b2
netzon-eladio <eladio.sareno@netzontech.com>
2020-11-14 01:04:07 +0800

    chore(eol): End of lecture

--- src/com/luv2code/springdemo/beanannotation/AnnotationPingPongCoach.java
+++ src/com/luv2code/springdemo/beanannotation/AnnotationPingPongCoach.java
@@ -2,12 +2,14 @@ package com.luv2code.springdemo.beanannotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.luv2code.springdemo.Coach;
 import com.luv2code.springdemo.FortuneService;
 
 @Component
+@Scope("singleton")
 public class AnnotationPingPongCoach implements Coach {
 	
 	@Autowired
