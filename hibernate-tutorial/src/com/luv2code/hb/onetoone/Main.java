89d773283c153956e565365cc88f284d19642e1e
netzon-eladio <eladio.sareno@netzontech.com>
2020-11-15 00:50:17 +0800

    chore(eol): End of lecture

--- hibernate-tutorial/src/com/luv2code/hb/onetoone/Main.java
+++ hibernate-tutorial/src/com/luv2code/hb/onetoone/Main.java
@@ -1,10 +1,41 @@
 package com.luv2code.hb.onetoone;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SessionFactory sessionFactory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		Session session;
+		
+		try {
+			session = sessionFactory.getCurrentSession();
 
+			Instructor tmpInstructor = new Instructor("InsJohn", "InsDoe", "insjdoe@example.com");
+			InstructorDetail tmpInstructorDetail = new InstructorDetail("www.youtube.com/hb", "coding java");
+			tmpInstructor.setInstructorDetail(tmpInstructorDetail);
+
+			session.beginTransaction();
+			
+			session.save(tmpInstructor);
+			
+			session.getTransaction().commit();
+			
+			System.out.println("Instructor saved!");
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			sessionFactory.close();
+		}
 	}
 
 }
